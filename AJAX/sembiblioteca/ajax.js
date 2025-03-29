7eb54a96dd0bba655ed1485bfc5c5ec8d4462139
yurivianadev <yuriferreira620@gmail.com>
2025-03-29 15:44:27 -0300

    AJAX sem biblioteca com JS puro

---
+++ AJAX/sembiblioteca/ajax.js
@@ -0,0 +1,34 @@
+/*var xhr = new XMLHttpRequest()
+var documento = {
+        "userId": 30,
+        "id": 13,
+        "title": "Noticia Exclusiva",
+        "body": "Developer larga Jquery"
+}
+xhr.onreadystatechange = function(){
+
+    if(xhr.readyState == 4){
+        console.log(xhr)
+
+    }
+}
+
+xhr.open("POST","https://jsonplaceholder.typicode.com/posts")
+
+xhr.send(documento)*/
+
+/*let xhr = new XMLHttpRequest()
+xhr.open('POST','https://jsonplaceholder.typicode.com/posts')
+
+xhr.setRequestHeader('Content-Type', 'application/json;charset=UTF-8')
+
+xhr.onprogress = function(){
+    console.log('Carregando')
+}
+xhr.onload = function(){
+    var obj = JSON.parse(xhr.responseText)
+    console.log(obj)
+}
+xhr.send()*/
+
+
